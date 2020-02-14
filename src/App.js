393f549f7782cea0047a4a6f410869a3e9080140
dev.reginaldo@gmail.com <dev.reginaldo@gmail.com>
2020-02-14 13:53:40 -0200

    Config navigation with React navigation Dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import Routes from "./routes";
+
 import Header from './components/Header';
-import Main from './pages/main'
+
 
 import './styles.css';
 
@@ -9,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Main />
+        <Routes />
       </div>
     );
   }
